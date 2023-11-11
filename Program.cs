c9acb353ec344a42dc0f1fbefc4e15343ca77663
NatallyHristova <137000118+NatallyHristova@users.noreply.github.com>
2023-11-11 13:26:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+string text = Console.ReadLine();
+
+StringBuilder digit = new StringBuilder();
+
+
+StringBuilder letters = new StringBuilder();
+
+
+StringBuilder otherCharacters = new StringBuilder();
+
+foreach(char symbol in text)
+{
+    if (char.IsDigit(symbol))
+    {
+        digit.Append(symbol);
+    }
+    else if (char.IsLetter(symbol))
+    {
+        letters.Append(symbol);
+    }
+    else
+    {
+        otherCharacters.Append(symbol);
+    }
+
+}
+
+Console.WriteLine(digit);
+Console.WriteLine(letters);
+Console.WriteLine(otherCharacters);
